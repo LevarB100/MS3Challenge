23974980045ce69c0264ac498168dbf5adb0313c
Levar Barrett <emaillevar@gmail.com>
2020-05-19 22:13:51 -0400

    initial commit

---
+++ src/AccountHolder/Account.java
@@ -0,0 +1,94 @@
+package AccountHolder;
+
+public class Account {
+  
+  public String A;
+  public String B;
+  public String C;
+  public String D;
+  public String E;
+  public String F;
+  public String G;
+  public String H;
+  public String I;
+  public String J;
+  public String getA() {
+    return A;
+  }
+  public void setA(String a) {
+    A = a;
+  }
+  public String getB() {
+    return B;
+  }
+  public void setB(String b) {
+    B = b;
+  }
+  public String getC() {
+    return C;
+  }
+  public void setC(String c) {
+    C = c;
+  }
+  public String getD() {
+    return D;
+  }
+  public void setD(String d) {
+    D = d;
+  }
+  public String getE() {
+    return E;
+  }
+  public void setE(String e) {
+    E = e;
+  }
+  public String getF() {
+    return F;
+  }
+  public void setF(String f) {
+    F = f;
+  }
+  public String getG() {
+    return G;
+  }
+  public void setG(String g) {
+    G = g;
+  }
+  public String getH() {
+    return H;
+  }
+  public void setH(String h) {
+    H = h;
+  }
+  public String getI() {
+    return I;
+  }
+  public void setI(String i) {
+    I = i;
+  }
+  public String getJ() {
+    return J;
+  }
+  public void setJ(String j) {
+    J = j;
+  }
+  
+  
+  public Account(String a, String b, String c, String d, String e, String f, String g, String h,
+      String i, String j) {
+    super();
+    A = a;
+    B = b;
+    C = c;
+    D = d;
+    E = e;
+    F = f;
+    G = g;
+    H = h;
+    I = i;
+    J = j;
+  }
+  
+  
+
+}
